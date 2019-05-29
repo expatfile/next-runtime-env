b84de6ed9134c5163ad6df9e665010c8ed82e16b
Andrew McLagan <andrewmclagan@gmail.com>
2019-05-29 15:48:42 +1000

    less arch builds

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ set -e
 
 # publish nginx image
 cd packages/nginx
+docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD
+docker build -t beamaustralia/react-env:$TRAVIS_TAG -t beamaustralia/react-env:latest .  
 docker push beamaustralia/react-env:latest
 docker push beamaustralia/react-env:$TRAVIS_TAG
 
