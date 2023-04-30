195a1717515414b6d7124a4dfd284ee933e3b975
Zino Hofmann <zino@hofmann.amsterdam>
2023-04-30 21:12:25 +0000

    chore: 🔧 update config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,49 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "airbnb-base",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "plugin:prettier/recommended",
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
   },
   env: {
     node: true,
     jest: true,
   },
   rules: {
-    "eol-last": ["error", "always"],
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-    "import/extensions": ["off"],
-    "import/prefer-default-export": ["off"],
-    "simple-import-sort/imports": "warn",
-    "simple-import-sort/exports": "warn",
-    "@typescript-eslint/no-empty-interface": ["off"],
-    "@typescript-eslint/explicit-function-return-type": ["off"],
+    'eol-last': ['error', 'always'],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'import/extensions': ['off'],
+    'import/prefer-default-export': ['off'],
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+    '@typescript-eslint/no-empty-interface': ['off'],
+    '@typescript-eslint/explicit-function-return-type': ['off'],
   },
   overrides: [
     {
-      files: ["src/__mocks__/.*", "**/*.spec.ts"],
+      files: ['src/__mocks__/.*', '**/*.spec.ts'],
       rules: {
-        "import/no-extraneous-dependencies": [
-          "error",
+        'import/no-extraneous-dependencies': [
+          'error',
           { devDependencies: true },
         ],
-        "@typescript-eslint/no-empty-function": ["off"],
-        "@typescript-eslint/no-non-null-assertion": ["off"],
+        '@typescript-eslint/no-empty-function': ['off'],
+        '@typescript-eslint/no-non-null-assertion': ['off'],
       },
     },
   ],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".ts"],
+        extensions: ['.js', '.ts'],
       },
     },
   },
-  plugins: ["simple-import-sort"],
+  plugins: ['simple-import-sort'],
 };
