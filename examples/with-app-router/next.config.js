95ecb0f64756c3d2c7d2b2a167da037a8553bfed
Zino Hofmann <zino@hofmann.amsterdam>
2023-10-11 18:23:25 +0000

    docs: ⬆️ upgrade examples

--- examples/with-app-router/next.config.js
+++
@@ -1,8 +0,0 @@
-const { configureRuntimeEnv } = require('next-runtime-env/build/configure');
-
-configureRuntimeEnv();
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
