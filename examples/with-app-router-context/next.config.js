105d88662707a0bc26f6e7fb98297e90ae27a84e
Zino Hofmann <zino@hofmann.amsterdam>
2024-05-16 14:42:18 +0000

    fix: 🩹 context approach requires `serverComponentsExternalPackages`

--- examples/with-app-router-context/next.config.js
+++ examples/with-app-router-context/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    serverComponentsExternalPackages: ['next-runtime-env'],
     // This is optional incase you want to make some private env vars publicly
     // available.
     instrumentationHook: true,
